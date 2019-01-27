5d3ace1bf3d1eff6a9e5fcfafd5af1fc99a41f1f
jwr <johnrobison83@gmail.com>
2019-01-26 19:00:15 -0500

    add an echo $SUCCESS

--- dockerfile-linter.sh
+++ dockerfile-linter.sh
@@ -28,5 +28,7 @@ if [ $SUCCESS -ne 0 ]; then
   PAYLOAD=$(echo '{}' | jq --arg body "$OUTPUT" '.body = $body')
   COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
   curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
+else
+	echo $DOCKERFILE linting exited $SUCCESS
 fi
 exit $SUCCESS
