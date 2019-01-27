d6fd7743bcca3c35644315eedb7c5ec5ecd30cfd
jwr <johnrobison83@gmail.com>
2019-01-27 12:27:45 -0500

    Initial merge to master (#1)
    * initial commit
    
    * add an echo $SUCCESS
    
    * remove the TOKEN stuff
    
    * echo starting the script
    
    * chmod 755
    
    * variable substitution
    
    * validate that DOCKERFILE exists
    
    * if DOCKERFILE is missing, exit
    
    * quotes around $DOCKERFILE
    
    * update README
    
    * add fromlatest.io link to README
    
    * punctuation

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM replicated/dockerfilelint:be1f746
+
+LABEL maintainer="John Robison <jwr@jwr.io>"
+LABEL "com.github.actions.name"="Dockerfile Linter"
+LABEL "com.github.actions.description"="Lint a Dockerfile, or many Dockerfiles"
+LABEL "com.github.actions.icon"="activity"
+LABEL "com.github.actions.color"="red"
+
+RUN	apk add --no-cache \
+	bash \
+	ca-certificates \
+	curl \
+	jq
+
+COPY dockerfile-linter.sh /usr/bin/
+RUN chmod 755 /usr/bin/dockerfile-linter.sh
+ENTRYPOINT ["dockerfile-linter.sh"]
---
+++ dockerfile-linter.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+if [[ -z "$GITHUB_TOKEN" ]]; then
+	echo "The GITHUB_TOKEN is required."
+	exit 1
+fi
+
+cd $GITHUB_WORKSPACE
+DOCKERFILE="${DOCKERFILE:-./Dockerfile}"
+if [[ ! -f $DOCKERFILE ]]; then
+	echo $DOCKERFILE not found. Exiting.
+	exit 1
+fi
+set +e
+OUTPUT=$(/dockerfilelint/bin/dockerfilelint "$DOCKERFILE")
+SUCCESS=$?
+echo $OUTPUT
+set -e
+# If there were errors as part of linting, post a comment. Else, do nothing.
+if [ $SUCCESS -ne 0 ]; then
+  PAYLOAD=$(echo '{}' | jq --arg body "$OUTPUT" '.body = $body')
+  COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
+  curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
+else
+	echo $DOCKERFILE linting exited $SUCCESS
+fi
+exit $SUCCESS
